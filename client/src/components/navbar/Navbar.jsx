11624fea1b43420aad06463a536606b38f11fb87
meir <lightir21@gmail.com>
2023-02-16 23:56:38 +0200

    navbar start

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="buttons">
+        <a href="#">ראשי</a>
+        <a href="#">אודות</a>
+        <a href="#">צרו קשר</a>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
